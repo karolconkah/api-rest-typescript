775db6163c41ba412ca1105f50f17529d64d94fb
Karoline Vieira Neves <karolconkah1@outlook.com>
2026-02-09 10:50:37 -0300

    added providers pessoas

---
+++ src/server/database/providers/pessoas/index.ts
@@ -0,0 +1,7 @@
+export { create } from './create';
+export { deleteById } from './deletebyid';
+export { updateById } from './updatebyid';
+export { getById } from './getbyid';
+export { getAll } from './getall';
+export { count } from './count'
+
